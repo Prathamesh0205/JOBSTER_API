8d261e1cec43c641f22ddcda7d7ee4d43edcc5ec
unknown <Prathamesh0205>
2023-06-23 21:38:45 +0530

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+const { register, login,updateUser } = require('../controllers/auth')
+const authenticateUser=require("../middleware/authentication")
+const testUser=require("../middleware/testUser");
+const  rateLimiter  = require('express-rate-limit');
+
+const apiLimiter=rateLimiter({
+    windowMs:15*60*1000,
+    max:10,
+    message:{
+        msg:'Too many requests from this IP,please try again 15 minutes later'
+    }
+})
+
+
+
+
+
+router.post('/register',apiLimiter,register)
+router.post('/login', apiLimiter,login)
+router.patch('/updateUser',authenticateUser,testUser,updateUser);
+module.exports = router
